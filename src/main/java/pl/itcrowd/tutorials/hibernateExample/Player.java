6ec51d3afb00c6bc15d5b8cad971c105be0968e9
Marcin <marcin.nowak@itcrowd.pl>
2013-01-18 15:00:10 +0100

    Add classes

---
+++ src/main/java/pl/itcrowd/tutorials/hibernateExample/Player.java
@@ -0,0 +1,11 @@
+package pl.itcrowd.tutorials.hibernateExample;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kleks
+ * Date: 1/18/13
+ * Time: 2:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Player {
+}
